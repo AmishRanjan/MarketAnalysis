42fb15f1cb278cc267dda35b956a60521d71f68e
AmishRanjan <amishranjanranjan@gmail.com>
2019-02-18 17:24:52 +0530

    working models added

--- scripts/helper/Widgets.py
+++ scripts/helper/Widgets.py
@@ -1,26 +1,28 @@
 ## This code contains several types of tkinter 
 ## windows that can be used for developing the user interface 
 
-from tkinter import Tk, Label, Button, Radiobutton, IntVar, Entry
+from tkinter import *
 
 class Widgets:
 
 	def __init__(self):
 		pass
 
-	def askToSelectOption(self, prompt, options):
+	def askToSelectOption(self, prompt, options, factor):
 		root = Tk()
 		root.configure(background="blue")
 		root.option_add("*Button.Background", "black")
 		root.option_add("*Button.Foreground", "red")
-		root.geometry("200x200")
+		a = len(prompt) * factor[0]
+		b = len(options) * factor[1]
+		root.geometry("%dx%d" % (a,b))
 		root.title(prompt)
 		v = IntVar()
 		for i, option in enumerate(options):
 			Radiobutton(root, text=option, font=("Times", 10, "bold"), 
 			 background="blue", highlightbackground="black", 
-			 variable=v, value=i).pack(anchor="w")
-		Button(text="Submit", command=root.destroy).pack()
+			 variable=v, value=i, width = a, indicatoron=0).pack(anchor="n")
+		Button(text="Submit", command=root.destroy).pack(anchor = "n")
 		root.mainloop()
 		return options[v.get()]
 
@@ -55,3 +57,29 @@ class Widgets:
 		master.mainloop()
 		
 		return self.values
+
+	def scrollbar(self, prediction_type_prompt, prediction_type_option):
+		root = Tk()
+		root.title(prediction_type_prompt)
+		root.option_add("*Button.Background", "black")
+		root.option_add("*Button.Foreground", "red")
+		scrollbar = Scrollbar(root, bg="blue", 
+				highlightbackground="black")
+		scrollbar.pack( side = "right", fill = "y" )
+
+		mylist = Listbox(root, yscrollcommand = scrollbar.set, bg="blue", 
+				highlightbackground="black", width=40, font=("Times", 10, "bold") )
+		for prediction_type in prediction_type_option:
+		   mylist.insert(END, prediction_type)
+
+		
+		self.prediction_type = []
+		def getValues():
+			self.prediction_type.append(mylist.get(ACTIVE))
+			root.destroy()
+
+		mylist.pack()
+		scrollbar.config( command = mylist.yview )
+		Button(text="Submit", command=getValues).pack(anchor = "s")
+		mainloop()
+		return self.prediction_type[0]
